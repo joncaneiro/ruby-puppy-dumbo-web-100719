db3f92eda68a9fc35afbbf247e13295e114f4460
Jon Caneiro <joncaneiro@gmail.com>
2019-10-09 18:43:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Add your code here
+require 'pry'
+
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  
+  def Dog.clear_all
+    @@all.clear
+  end
+  
+  def Dog.print_all
+    @@all.each {|dog_name| puts dog_name.name}
+  end
+  binding.pry
+end
